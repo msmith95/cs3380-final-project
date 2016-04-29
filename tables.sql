f3a2de1fe96b00c8ca3ee6be7bc271ad781475df
Michael Smith Jr <mdsf3f@mail.missouri.edu>
2016-04-28 20:14:57 -0500

    Updated table creation statements.

--- tables.sql
+++ tables.sql
@@ -1,70 +1,78 @@
+DROP TABLE IF EXISTS student_item_transaction;
+DROP TABLE IF EXISTS item_condition_update;
+DROP TABLE IF EXISTS item_condition;
+DROP TABLE IF EXISTS item_has_category;
+DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS item_category;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS employee_permissions;
+DROP TABLE IF EXISTS location;
+DROP TABLE IF EXISTS waiver;
+DROP TABLE IF EXISTS student;
+
+
 CREATE TABLE student(
-	id int(8) PRIMARY KEY,
-	username VARCHAR(16),
+	id int PRIMARY KEY,
+	pawprint VARCHAR(16),
 	email VARCHAR(255),
 	name_first VARCHAR(30),
-	name_last VARCHAR(45)
+	name_last VARCHAR(45),
+	password VARCHAR(255),
+	salt VARCHAR(255),
+	privilege VARCHAR(255)
 ) ENGINE = INNODB;
 
 CREATE TABLE waiver(
-	id int(2) PRIMARY KEY,
-	name VARCHAR(250)
-) ENGINE = INNODB;
-
-CREATE TABLE student_has_waiver(
-	student_id int(8),
-	waiver_id int(2),
-	FOREIGN KEY(student_id) references student(id),
-	FOREIGN KEY(waiver_id) references waiver(id),
-	PRIMARY KEY(student_id, waiver_id)
+	student_id int PRIMARY KEY,
+	FOREIGN KEY(student_id) references student(id)
 ) ENGINE = INNODB;
 
 CREATE TABLE location(
-	id int(2) PRIMARY KEY,
+	id int PRIMARY KEY,
 	name VARCHAR(250)
 ) ENGINE = INNODB;
 
 CREATE TABLE employee_permissions(
-	id int(1) PRIMARY KEY,
+	id int PRIMARY KEY,
 	name VARCHAR(250)
 ) ENGINE = INNODB;
 
 CREATE TABLE employee(
-	id int(8) PRIMARY KEY,
+	id int PRIMARY KEY,
 	username VARCHAR(16),
 	email VARCHAR(255),
 	name_first VARCHAR(30),
 	name_last VARCHAR(45),
-	permissions int(1),
+	permissions int,
 	FOREIGN KEY(permissions) references employee_permissions(id)
 ) ENGINE = INNODB;
 
 CREATE TABLE item_category(
-	id int(5) PRIMARY KEY,
+	id int PRIMARY KEY,
 	name VARCHAR(250)
 ) ENGINE = INNODB;
 
 CREATE TABLE item(
-	id int(10) PRIMARY KEY,
+	id varchar(50) PRIMARY KEY,
 	name VARCHAR(250)
 ) ENGINE = INNODB;
 
 CREATE TABLE item_has_category(
-	item_id int(10),
-	category int(5),
+	item_id varchar(50),
+	category int,
 	FOREIGN KEY(item_id) references item(id),
 	FOREIGN KEY(category) references item_category(id),
 	PRIMARY KEY(item_id, category)
 ) ENGINE = INNODB;
 
 CREATE TABLE item_condition(
-	id int(2) PRIMARY KEY,
+	id int PRIMARY KEY,
 	name VARCHAR(250)
 ) ENGINE = INNODB;
 
 CREATE TABLE item_condition_update(
-	item_id int(10),
-	item_condition int(2),
+	item_id varchar(50),
+	item_condition int,
 	update_time datetime,
 	FOREIGN KEY(item_id) references item(id),
 	FOREIGN KEY(item_condition) references item_condition(id),
@@ -72,16 +80,18 @@ CREATE TABLE item_condition_update(
 ) ENGINE = INNODB;
 
 CREATE TABLE student_item_transaction(
-	student_id int(8),
-	employee_id int(8),
-	location int(2),
-	item_id int(10),
-	item_condition int(2),
-	transaction_time datetime,
+	student_id int,
+	employee_id int,
+	location int,
+	item_id varchar(50),
+	item_condition int,
+	check_out_time datetime,
+	due_time datetime,
+	check_in_time datetime,
 	FOREIGN KEY(student_id) references student(id),
 	FOREIGN KEY(employee_id) references employee(id),
 	FOREIGN KEY(location) references location(id),
 	FOREIGN KEY(item_id) references item(id),
 	FOREIGN KEY(item_condition) references item_condition(id),
-	PRIMARY KEY(student_id, employee_id, location, item_id, item_condition, transaction_time)
+	PRIMARY KEY(student_id, employee_id, location, item_id, item_condition, check_out_time, due_time, check_in_time)
 ) ENGINE = INNODB;
